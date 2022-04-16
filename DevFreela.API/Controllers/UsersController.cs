14ea1bd668eb181f67eade441cb6e83e4330e17a
LVIANACODE <lucas_viana99@outlook.com>
2022-04-16 05:49:54 -0300

    initial project

---
+++ DevFreela.API/Controllers/UsersController.cs
@@ -0,0 +1,35 @@
+﻿using DevFreela.API.Models;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DevFreela.API.Controllers
+{
+    [Microsoft.AspNetCore.Components.Route("api/users")]
+    public class UsersController : ControllerBase
+    {
+        public UsersController()
+        {
+                
+        }
+
+
+        [HttpGet]
+        public IActionResult GetById(int id)
+        {
+            return Ok();
+        }
+
+
+        [HttpPost]
+        public IActionResult Post([FromBody] CreateUserModel createUserModel)
+        {
+            return CreatedAtAction(nameof(GetById), new { id = 1}, createUserModel);
+        }
+
+        [HttpPut]
+        public IActionResult Login(int id,[FromBody] LoginModel login)
+        {
+            return NoContent();
+        }
+    }
+}
