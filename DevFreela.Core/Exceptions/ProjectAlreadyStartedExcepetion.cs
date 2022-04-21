6194dd74311d4c5e6a241339d661d09547fffbd2
LVIANACODE <lucas_viana99@outlook.com>
2022-04-21 19:45:47 -0300

    Adding Domain Layer

---
+++ DevFreela.Core/Exceptions/ProjectAlreadyStartedExcepetion.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DevFreela.Core.Exceptions
+{
+    public class ProjectAlreadyStartedExcepetion : Exception
+    {
+        public ProjectAlreadyStartedExcepetion() :base("Project is already in Started status")
+        {
+
+        }
+    }
+}
