cc880869113ab4032f04367eaa5b5d6f1a4c0cd9
LVIANACODE <lucas_viana99@outlook.com>
2022-04-25 23:12:48 -0300

    Applying the infrastructure layer in the project

--- DevFreela.Core/Entities/Project.cs
+++ DevFreela.Core/Entities/Project.cs
@@ -6,7 +6,7 @@ namespace DevFreela.Core.Entities
 {
     public class Project : BaseEntity
     {
-        public Project(string title, int description, int idClient, int idFreelancer, decimal totalCost)
+        public Project(string title, string description, int idClient, int idFreelancer, decimal totalCost)
         {
             Title = title;
             Description = description;
@@ -20,7 +20,7 @@ namespace DevFreela.Core.Entities
         }
 
         public string Title { get; private set; }
-        public int Description { get; private set; }
+        public string Description { get; private set; }
         public int IdClient { get; private set; }
         public int IdFreelancer { get; private set; }
         public decimal TotalCost { get; private set; }
---
+++ DevFreela.Infrastructure/Persistence/Repositories/DevFreelaDbContext.cs
@@ -0,0 +1,41 @@
+﻿using DevFreela.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevFreela.Infrastructure.Persistence
+{
+    public class DevFreelaDbContext
+    {
+
+        public DevFreelaDbContext()
+        {
+            Projects = new List<Project>
+            {
+                new Project("Meu Projeto ASPNET Core 1", "Minha Descrição de Projeto 1", 1, 1, 1000),
+                new Project("Meu Projeto ASPNET Core 1", "Minha Descrição de Projeto 2", 1, 1, 20000),
+                new Project("Meu Projeto ASPNET Core 1", "Minha Descrição de Projeto 3", 1, 1, 30000)
+            };
+
+            Users = new List<User>
+            {
+                new User("Lucas Viana", "lucas@gmail.com", new DateTime(1999, 1,10)),
+                new User("Ronaldo Fenomeno", "lucas@gmail.com", new DateTime(2000, 1,10)),
+                new User("Cesar Filho", "lucas@gmail.com", new DateTime(1988, 1,10)),
+            };
+
+            Skills = new List<Skill>
+            {
+                new Skill(".NET Core"),
+                new Skill("C#"),
+                new Skill("SQL")
+            };
+        }
+
+        public List<Project> Projects { get; set; }
+        public List<User> Users { get; set; }
+        public List<Skill> Skills { get; set; }
+    }
+}
